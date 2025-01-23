72d465e67684db6dfe8f7a9122c4780660d8d200
Taetae1123 <xodus2079@naver.com>
2025-01-24 03:05:40 +0900

    Fix: 장바구니에 이미 담긴거 체크

--- JS/itemDetail.js
+++ JS/itemDetail.js
@@ -31,7 +31,7 @@ const getInfo = () => {
       <div class="imgbox"><img class="img" src="${x.img}" alt="선택된이미지" /></div>
         <div class="flexbox">
         <div class="title">${x.name}</div>
-        <button class="btn" onclick="getCart()">장바구니</button>
+        <button class="btn" onclick="checkAlert()">장바구니</button>
       </div>
       <div>${x.price}원</div>
       <div>${x.content}</div>
@@ -42,26 +42,52 @@ const getInfo = () => {
 getInfo();
 
 const getCart = () => {
-  let cartList = JSON.parse(localStorage.getItem("cartInfo"));
+  let cartList = JSON.parse(localStorage.getItem("cartInfo")) || [];
 
   // 배열을 빼고 넣어주기
   cartData.push(...cartList);
-  data.map((x, i) => {
+  data.map((x) => {
     if (x.id === value) {
-      // 데이터 넣기
-      let cartInfo = {
-        id: x.id,
-        img: x.img,
-        name: x.name,
-        price: x.price,
-        content: x.content,
-      };
-      cartData.push(cartInfo);
+      // 이미 있는 아이디인지 체크(filter는 새로운 배열로 반환, some은 true,false 반환)
+      const isInCart = cartData.some((item) => item.id === x.id);
+      if (!isInCart) {
+        // 데이터 넣기
+        let cartInfo = {
+          id: x.id,
+          img: x.img,
+          name: x.name,
+          price: x.price,
+          content: x.content,
+        };
+        cartData.push(cartInfo);
 
-      localStorage.setItem("cartInfo", JSON.stringify(cartData));
+        localStorage.setItem("cartInfo", JSON.stringify(cartData));
+        window.location.reload();
+      }
     }
   });
-  window.location.reload();
+};
+//담을건지 확인
+const checkAlert = () => {
+  Swal.fire({
+    title: "장바구니",
+    text: "장바구니에 담을까요??",
+    icon: "question",
+    showCancelButton: true,
+    confirmButtonColor: "#3085d6",
+    cancelButtonColor: "#d33",
+    confirmButtonText: "네",
+    cancelButtonText: "아니요",
+  }).then((result) => {
+    if (result.isConfirmed) {
+      Swal.fire({
+        title: "이미 담긴 상품입니다!",
+        text: "장바구니에서 확인해주세요",
+        icon: "success",
+      });
+      getCart();
+    }
+  });
 };
 
 // 장바구니 개수
